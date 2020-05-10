c0630f926e3b6268fba952d83f46a8c7131a17bc
Michael Gallup <Gallup93@icloud.com>
2020-05-10 02:22:51 -0600

    Add edit_spec with shelter#edit route and action.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
+  get '/shelters/:id/edit', to: 'shelters#edit'
   post '/shelters', to: 'shelters#create'
 end
---
+++ spec/features/user_edits_a_shelter_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe "I click on the 'Update Shelter' link" do
+    it "taken to '/shelters/:id/edit' where I see a form to edit the shelter's data" do
+      shelter_2 = Shelter.create(name: "Helter Shelter", address: "666 7th St",
+                                 city: "Chicago", state: "Illinois", zip: "60626")
+
+      visit "/shelters/#{shelter_2.id}/edit"
+      # expect(current_path).to eq("/shelters/#{shelter_2.id}")
+      # expect(page).to have_content("Edit Shelter")
+      # expect(page).to have_content(shelter_2.name)
+      click_on "Edit Shelter"
+
+      # expect(current_path).to eq("/shelters/#{shelter_2.id}/edit")
+      # expect(page).to have_content("Update Shelter: #{shelter_2.name}")
+
+      fill_in :name, with: "Heck'n Helter Shelter"
+      fill_in :address, with: "1060 W Addison"
+      fill_in :city, with: "Chicago"
+      fill_in :state, with: "Illinois"
+      fill_in :zip, with: "61108"
+
+      click_on "Update Shelter"
+
+      expect(current_path).to eq("/shelters/#{shelter_2.id}")
+      expect(page).to have_content("Heck'n Helter Shelter")
+      expect(page).to have_content("61108")
+    end
+  end

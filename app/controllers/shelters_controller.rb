10b7c1daa4886ca00135459bb0f9f6437e9853cd
Michael Gallup <Gallup93@icloud.com>
2020-05-09 23:17:42 -0600

    Add shelter show action index

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -2,4 +2,8 @@ class SheltersController < ApplicationController
   def index
     @shelters = Shelter.all
   end
+
+  def show
+    @shelter = Shelter.find{|shelter| shelter.id = params[:id]}
+  end
 end

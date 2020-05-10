a46b1a13014f2235b2b05c875fd050f379d8849a
Michael Gallup <Gallup93@icloud.com>
2020-05-09 23:39:47 -0600

    Add get shelters/new spec

--- spec/features/user_sees_all_shelters_spec.rb
+++ spec/features/user_sees_all_shelters_spec.rb
@@ -18,4 +18,41 @@ describe "user sees all shelters" do
       expect(page).to have_content(shelter_3.name)
     end
   end
+  describe "they click link 'New Shelter'" do
+    it "redirects them to /shelters/new" do
+      visit "/shelters"
+      click_on "New Shelter"
+      expect(current_path).to eq("/shelters/new")
+        it "shows a form to create a new shelter" do
+          fill_in :name, with: "Sage's Pigeon Pile"
+          fill_in :address, with: "536 Ratbird St"
+          fill_in :city, with: "Paris"
+          fill_in :state, with: "Florida"
+          fill_in :zip, with: "61735"
+
+          click_on "Create Shelter"
+          expect(current_path).to eq("/shelters")
+          expect(page).to have_content("Sage's Pigeon Pile")
+        end
+    end
+  end
 end
+
+
+# User Story 4, Shelter Creation
+#
+# As a visitor
+# When I visit the Shelter Index page
+# Then I see a link to create a new Shelter, "New Shelter"
+# When I click this link
+# Then I am taken to '/shelters/new' where I  see a form for a new shelter
+# When I fill out the form with a new shelter's:
+# - name
+# - address
+# - city
+# - state
+# - zip
+# And I click the button "Create Shelter" to submit the form
+# Then a `POST` request is sent to '/shelters',
+# a new shelter is created,
+# and I am redirected to the Shelter Index page where I see the new Shelter listed.
